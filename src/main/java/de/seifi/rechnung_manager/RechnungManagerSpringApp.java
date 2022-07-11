41e09b0d3a07c77ca79d22be0d80cc2e6be61135
Hamidreza Seifi <Hamidreza.Seifi@hdi.de>
2022-07-11 10:03:36 +0200

    Make app single instance

--- src/main/java/de/seifi/rechnung_manager/RechnungManagerSpringApp.java
+++ src/main/java/de/seifi/rechnung_manager/RechnungManagerSpringApp.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import it.sauronsoftware.junique.AlreadyLockedException;
+import it.sauronsoftware.junique.JUnique;
+import it.sauronsoftware.junique.MessageHandler;
 import javafx.application.Application;
 
 @SpringBootApplication
@@ -11,8 +14,32 @@ public class RechnungManagerSpringApp {
 	public static ConfigurableApplicationContext applicationContext;
 	
 	public static void main(String[] args) {
-		applicationContext = SpringApplication.run(RechnungManagerSpringApp.class, args);
-		Application.launch(RechnungManagerFxApp.class, args);
+		
+		String appId = "RechnungManagerSpringAppId";
+	    boolean alreadyRunning;
+	    try {
+	        JUnique.acquireLock(appId, new MessageHandler() {
+	            public String handle(String message) {
+	                // A brand new argument received! Handle it!
+	                return null;
+	            }
+	        });
+	        alreadyRunning = false;
+	    } catch (AlreadyLockedException e) {
+	        alreadyRunning = true;
+	    }
+	    if (!alreadyRunning) {
+	    	
+	    	applicationContext = SpringApplication.run(RechnungManagerSpringApp.class, args);
+			Application.launch(RechnungManagerFxApp.class, args);
+			
+	    } else {
+	        for (int i = 0; i < args.length; i++) {
+	            JUnique.sendMessage(appId, args[0]);
+	        }
+	    }
+	    
+		
 	}
 
 }
